1878972f4793f48494b28045db45ec33b83bd368
Prabhjot Singh Sains <prabhjot.sains@gmail.com>
2023-08-17 00:10:51 -0400

    Added stuff in data.php

--- app/Models/Data.php
+++ app/Models/Data.php
@@ -11,5 +11,5 @@ class Data extends Model
     protected $collection = 'Portfolio__Data'; // Name of your MongoDB collection
     protected $connection = 'mongodb'; // Connection name from your database configuration
 
-    protected $fillable = ['title', 'icon', 'image', 'link', 'languages', 'description'];
+    protected $fillable = ['title', 'icon', 'image', 'link', 'languages', 'description', 'company__name', 'company__address', 'work__time', 'position_title', 'position__description'];
 }
